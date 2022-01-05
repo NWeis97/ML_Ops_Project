6931ac22d5ea10d765c04cc5db66734a842969dd
NWeis97 <nicolai.weisbjerg@gmail.com>
2022-01-05 22:04:58 +0100

    Addition of config files and automation of layers in model structure - not completely done, but is working

--- src/models/model.py
+++ src/models/model.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from torch import nn, optim
+from torch.nn.modules.pooling import MaxPool2d
 from torchvision import datasets, transforms
 
 
@@ -43,3 +44,82 @@ class ConvolutionModel_v1(nn.Module):
         x = F.log_softmax(self.fc2(x), dim=1)
 
         return x
+
+
+
+class CNNModuleVar(nn.Module):
+    """
+    Deep neural network for the mnist dataset using convolutions then fully connected layers
+    """
+    def __init__(self, input_channel, conv_to_linear_dim, output_dim, 
+                 hidden_channel_array = [], hidden_kernel_array = [], 
+                 hidden_stride_array = [], hidden_padding_array = [], 
+                 hidden_dim_array = [], non_linear_function_array = [],
+                 regularization_array = []):
+        super().__init__()
+        
+        #Initialize lists
+        self.conv_functions = []
+        self.linear_functions = []
+        
+        #Extract number of layers
+        self.hidden_conv_layers = len(hidden_channel_array)
+        self.hidden_linear_layers = len(hidden_dim_array)
+        
+        #Extract activation functions - not implemented yet
+        #self.non_linear_functions_conv = [x() for x in non_linear_function_array[:self.hidden_conv_layers]]
+        #self.non_linear_functions_linear = [x() for x in non_linear_function_array[self.hidden_conv_layers:]]
+
+        #Extract reguralizers - not implemented yet
+        #self.regularization_conv = [x() for x in regularization_array[:self.hidden_conv_layers]]
+        #self.regularization_linear = [x() for x in regularization_array[self.hidden_conv_layers:]]
+
+        # Put convolutions in list
+        for l in range(self.hidden_conv_layers):
+            self.conv_functions.append(nn.Conv2d(
+                    in_channels=input_channel, 
+                    out_channels=hidden_channel_array[l], 
+                    kernel_size=hidden_kernel_array[l], 
+                    stride=hidden_stride_array[l], 
+                    padding=hidden_padding_array[l]
+            ))
+            input_channel = hidden_channel_array[l]
+        self.conv_functions = nn.ModuleList(self.conv_functions)
+
+        # Put fully connected layers in list
+        input_dim = conv_to_linear_dim
+        for l in range(self.hidden_linear_layers):
+            self.linear_functions.append(nn.Linear(input_dim, hidden_dim_array[l]))
+            input_dim = hidden_dim_array[l]
+        self.linear_functions = nn.ModuleList(self.linear_functions)
+        self.final_linear = nn.Linear(input_dim, output_dim)
+
+        #Define regularizers
+        self.MaxPool = nn.MaxPool2d((2, 2))
+        self.Dropout = nn.Dropout(p=0.2)
+
+
+    def forward(self, x):
+        out = x.view(x.shape[0], 1, x.shape[1], x.shape[2])
+        
+        # Run convolutional layers
+        for i in range(self.hidden_conv_layers):
+            out = self.conv_functions[i](out)
+            #out = self.non_linear_functions_conv[i](out)
+            out = F.leaky_relu(out)
+            out = self.MaxPool(out)
+
+        # Flatten
+        out = out.view(out.shape[0], -1)
+
+        # Run fully connected layers
+        for i in range(self.hidden_linear_layers):
+            out = self.linear_functions[i](out)
+            #out = self.non_linear_functions_linear[i](out)
+            out = F.leaky_relu(out)
+            out = self.Dropout(out)
+
+        # Final layer and classification
+        out = F.log_softmax(self.final_linear(out),dim=1)
+
+        return out
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -23,55 +23,89 @@ import torch
 from torch import nn, optim
 
 import os
-
-from model import ConvolutionModel_v1
-
+import pdb
+from model import ConvolutionModel_v1, CNNModuleVar
 sns.set_style("whitegrid")
 
-import pdb
+import hydra
+from hydra import compose, initialize
+from omegaconf import OmegaConf,DictConfig
 
-def main():
-    ###################################################
-    #################### Arguments ####################
-    ###################################################
-    # Arguments to be called
-    parser = argparse.ArgumentParser(description="Training arguments")
-    parser.add_argument("--lr", default=0.003)
-    parser.add_argument("--epochs", default=30)
-    parser.add_argument("--batchsize", default=64)
-    parser.add_argument("--modelType", default="ConvolutionModel_v1")
 
-    # Save arguments in args
-    args = parser.parse_args(sys.argv[1:])
-    print(args)
+import logging
+log = logging.getLogger(__name__)
 
+
+
+def build_model():
+    initialize(config_path="../../configs/", job_name="model")
+    cfg = compose(config_name="model.yaml")
+    log.info(f"configuration: \n {OmegaConf.to_yaml(cfg)}")
+    configs = cfg['hyperparameters']
 
     ###################################################
     ################# Hyperparameters #################
     ###################################################
-    # Define models, loss-function and optimizer
-    if args.modelType == "ConvolutionModel_v1":
-        model = ConvolutionModel_v1()
-    else:
-        print("Model type not found")
-    epochs = args.epochs
+    input_channel = configs['input_channel']
+    conv_to_linear_dim = configs['conv_to_linear_dim']
+    output_dim = configs['output_dim']
+    hidden_channel_array = configs['hidden_channel_array']
+    hidden_kernel_array = configs['hidden_kernel_array']
+    hidden_stride_array = configs['hidden_stride_array']
+    hidden_padding_array = configs['hidden_padding_array']
+    hidden_dim_array = configs['hidden_dim_array']
+    non_linear_function_array = configs['non_linear_function_array']
+    regularization_array = configs['regularization_array']
 
+    # Define models, loss-function and optimizer
+    model = CNNModuleVar(input_channel, conv_to_linear_dim,
+                        output_dim,hidden_channel_array,
+                        hidden_kernel_array,hidden_stride_array,
+                        hidden_padding_array,hidden_dim_array,
+                        non_linear_function_array,regularization_array)
+
+    return model
+
+
+
+def train():
+    # Get model struct
+    model = build_model()
+
+    cfg = compose(config_name="training.yaml")
+    log.info(f"configuration: \n {OmegaConf.to_yaml(cfg)}")
+    configs = cfg['hyperparameters']
+    
+    ###################################################
+    ################# Hyperparameters #################
+    ###################################################
+    batch_size = configs['batch_size']
+    lr = configs['lr']
+    epochs = configs['epochs']
+    seed = configs['seed']
+
+    # Set seed
+    torch.manual_seed(seed)
+
+    # Set name
+    modelType = 'CNNModuleVar'
 
     ###################################################
     ################### Load data #####################
     ###################################################
     # Load data and put in DataLoader
     Train = torch.load("data/processed/train_dataset.pt")
-    train_set = torch.utils.data.DataLoader(Train, batch_size=args.batchsize, shuffle=True)
+    train_set = torch.utils.data.DataLoader(Train, batch_size=batch_size, shuffle=True)
 
     # Set loss-function and optimizer
     criterion = nn.NLLLoss()
-    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
 
 
     ###################################################
     ################## Run training ###################
     ###################################################
+    epochs = epochs
     model.train()
     train_losses = []
     for e in range(epochs):
@@ -91,22 +125,22 @@ def main():
 
         else:
             train_losses.append(running_loss / len(train_set))
-            print("epoch: ", str(e), "/", str(epochs))
-            print("Training_loss: ", str(train_losses[e]))
-            print("")
+            log.info("epoch: ", str(e), "/", str(epochs))
+            log.info("Training_loss: ", str(train_losses[e]))
+            log.info("")
 
     # Save model
     os.makedirs("./models/fits/", exist_ok=True) #Create if not already exist
     torch.save(
         model,
         "models/fits/"
-        + args.modelType
+        + modelType
         + "_lr"
-        + str(args.lr)
+        + str(lr)
         + "_e"
-        + str(args.epochs)
+        + str(epochs)
         + "_bs"
-        + str(args.batchsize)
+        + str(batch_size)
         + ".pth",
     )
 
@@ -116,16 +150,16 @@ def main():
     os.makedirs("./reports/figures/training_loss/", exist_ok=True) #Create if not already exist
     plt.savefig(
         "reports/figures/training_loss/"
-        + args.modelType
+        + modelType
         + "_lr"
-        + str(args.lr)
+        + str(lr)
         + "_e"
-        + str(args.epochs)
+        + str(epochs)
         + "_bs"
-        + str(args.batchsize)
+        + str(batch_size)
         + ".png"
     )
 
 
 if __name__ == "__main__":
-    main()
+    train()

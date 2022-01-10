2a851cbebf24c02c78495d2b944261d2015091fb
NWeis97 <nicolai.weisbjerg@gmail.com>
2022-01-10 15:31:02 +0100

    New test

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,19 +3,26 @@ import pdb
 import os.path
 import pytest
 
-# content of test_sample.py
-Train = torch.load("data/processed/train_dataset.pt")
-Test = torch.load("data/processed/test_dataset.pt")
-N_Train = 40000
-N_Test = 5000
 
 @pytest.mark.skipif(not (os.path.exists("data/processed/train_dataset.pt") or 
                          os.path.exists("data/processed/test_dataset.pt")), reason="Data files not found")
 def test_num_obs():
+    # content of test_sample.py
+    Train = torch.load("data/processed/train_dataset.pt")
+    Test = torch.load("data/processed/test_dataset.pt")
+    N_Train = 40000
+    N_Test = 5000
+
     assert len(Train) == N_Train 
     assert len(Test) == N_Test
 
+@pytest.mark.skipif(not (os.path.exists("data/processed/train_dataset.pt") or 
+                         os.path.exists("data/processed/test_dataset.pt")), reason="Data files not found")
 def test_image_shape():
+    # content of test_sample.py
+    Train = torch.load("data/processed/train_dataset.pt")
+    Test = torch.load("data/processed/test_dataset.pt")
+
     for i in range(len(Train)):
         assert Train.__getitem__(i)[0].shape[0] == 28
         assert Train.__getitem__(i)[0].shape[1] == 28
@@ -24,8 +31,13 @@ def test_image_shape():
         assert Test.__getitem__(i)[0].shape[0] == 28
         assert Test.__getitem__(i)[0].shape[1] == 28
 
-
+@pytest.mark.skipif(not (os.path.exists("data/processed/train_dataset.pt") or 
+                         os.path.exists("data/processed/test_dataset.pt")), reason="Data files not found")
 def test_labels():
+    # content of test_sample.py
+    Train = torch.load("data/processed/train_dataset.pt")
+    Test = torch.load("data/processed/test_dataset.pt")
+
     for i in range(len(Train)):
         assert type(Train.__getitem__(i)[1]) == torch.Tensor
         assert type(Train.__getitem__(i)[1].item()) == int

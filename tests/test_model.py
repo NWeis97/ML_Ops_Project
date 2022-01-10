467faf0d01c2c954b7bc87ff20790a889f7700bd
NWeis97 <nicolai.weisbjerg@gmail.com>
2022-01-10 15:45:27 +0100

    New test v4.0

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,9 +5,16 @@ from omegaconf import OmegaConf,DictConfig
 from hydra.core.global_hydra import GlobalHydra
 import pytest
 
+import os
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname + '/..')
 
+from unittest import mock
+# Importing source_code.py
 import sys
-sys.path.append("../")
+#sys.set
+#sys.path.append("src")
 from src.models import train_model
 from src.models.model import ConvolutionModel_v1, CNNModuleVar
 from src.models.train_model import build_model
--- tests/test_train.py
+++ tests/test_train.py
@@ -5,10 +5,17 @@ from omegaconf import OmegaConf,DictConfig
 from hydra.core.global_hydra import GlobalHydra
 import pytest
 
+import os
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname + '/..')
+
 from unittest import mock
 # Importing source_code.py
 import sys
-sys.path.append("src")
+#sys.set
+#sys.path.append("src")
 from src.models import train_model
 from src.models.model import ConvolutionModel_v1, CNNModuleVar
 from src.models.train_model import build_model
